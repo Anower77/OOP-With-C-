8773b3bb3b1504b103ef86f8b58a81501693db65
anower77 <anowerhossain765562@gmail.com>
2026-02-06 08:32:05 +0600

    Chapter - 03 V.0.0.1

---
+++ Chapter - 02/Ex-41.cpp
@@ -0,0 +1,14 @@
+// Different types of literals
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 10;
+    long l = 1000000;
+    double d = -0.00001;
+
+    cout << i << ' ' << l << ' ' << d << '\n';
+
+    return 0;
+}
